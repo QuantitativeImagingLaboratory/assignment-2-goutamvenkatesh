5da06ae9387f3bedadf4308f545293ebd45c282a
goutamvenkatesh <goutam.venkatesh@gmail.com>
2017-10-31 21:53:09 -0500

    updated dft

--- DFT/DFT.py
+++ DFT/DFT.py
@@ -9,11 +9,38 @@ class DFT:
         takes as input:
         matrix: a 2d matrix
         returns a complex matrix representing fourier transform"""
+        
+        input_matrix = np.int_(rand(15, 15) * 256)
+        outputDFT=np.zeros((15,15), dtype=complex)
+        outputIFT=np.zeros((15,15), dtype=complex)
+        outMat=np.zeros((15,15))
+        
+
+        for u in range(0,15):
+            for v in range(0,15):
+                total_sum = 0
+                for i in range(0,15):
+                    for j in range(0,15):
+                        t=u*i
+                        k=v*j
+                        y=t + k
+                        x=2.0 * math.pi * y
+                        angle = x/ 15
+                        expcal=math.cos(angle) - 1j *math.sin(angle)
+                        total_sum += input[i,j] * expcal
+                    
+
+            
+                output[u,v]=sumValue
+
+            
+
+    return output
 
 
 
 
-        return matrix
+        
 
     def inverse_transform(self, matrix):
         """Computes the inverse Fourier transform of the input matrix
